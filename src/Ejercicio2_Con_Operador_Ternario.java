2109b6db55571b0dbf7cf6b8e9911a7d66c9a01c
Maximiliano <leonico@gmail.com>
2026-04-14 22:55:39 -0300

    Ejercicio 2 con operador ternario

---
+++ src/Ejercicio2_Con_Operador_Ternario.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejercicio2_Con_Operador_Ternario {
+    static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        System.out.print("Ingrese un valor: ");
+        int valor = leer.nextInt();
+        int edad=2;
+
+        String mensaje= (valor % 2==0) ? "Es PAR" : "Es IMPAR";
+        System.out.println(mensaje);
+    }
+}
