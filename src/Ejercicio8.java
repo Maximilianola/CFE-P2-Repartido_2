700a173c1535a515d4d462266a0676d70e0a7bca
Maximiliano <leonico@gmail.com>
2026-04-14 21:01:54 -0300

    Ejercicio 8 corregido

--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -1,3 +1,5 @@
+import com.sun.source.tree.IfTree;
+
 import java.util.Scanner;
 
 public class Ejercicio8 {
@@ -8,14 +10,25 @@ public class Ejercicio8 {
         int cant = leer.nextInt();
         int suma =0;
         int cont=0;
+        int aux=0;
 
         if (cant<=0){
             System.out.println("No se realizo ninguna operación.");
         }else {
+
             while (cant != cont) {
                 cont = cont + 1;
                 System.out.print("Ingrese número " + cont + ": ");
-                suma = suma + leer.nextInt();
+                aux = leer.nextInt();
+
+                while (aux<0){
+                    System.out.println("No es un número POSITIVO.");
+                    System.out.print("Ingrese número " + cont + ": ");
+                    aux = leer.nextInt();
+
+                }
+
+                suma=suma+aux;
             }
         System.out.println();
         System.out.println("La suma total es: " + suma);
